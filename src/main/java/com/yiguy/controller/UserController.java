530b8373ecb87081494fa7da2490b1b04c197be1
suxiaoyi <1132276272@qq.com>
2017-12-21 19:13:31 +0800

    delete: remove package for learning

--- src/main/java/com/yiguy/controller/UserController.java
+++ src/main/java/com/yiguy/controller/UserController.java
@@ -1,8 +1,5 @@
 package com.yiguy.controller;
 
-import java.sql.Timestamp;
-import java.util.UUID;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,43 +10,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yiguy.model.User;
 import com.yiguy.service.IBaseService;
-import com.yiguy.service.impl.UserService;
-
-import ilv.dao.impl.UserDaoImpl;
-import ilv.model.IlvUser;
-import ilv.service.UserServiceI;
-import ilv.service.UserServiceImpl;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
- 
-	/*private UserServiceI userService;
-	
-	public UserServiceI getUserService() {
-	    return userService;
-	}
 
-	@Autowired
-	public void setUserService(UserServiceI userService) {
-	    this.userService = userService;
-	}
-	
-	@RequestMapping(value="/test", method=RequestMethod.GET)
-	@ResponseBody
-	public void addUser() {
-        IlvUser user = new IlvUser();
-        // 设置UUID主键
-        user.setId(UUID.randomUUID().toString());
-        user.setRealname("yanglang");
-        user.setUsername("superadmin");
-        user.setPassword("hello");
-        user.setIslock("1");
-        user.setStatus("1");
-        user.setCreatetime(new Timestamp(System.currentTimeMillis()));
-        userService.save(user);
-    }*/
-	
 	private IBaseService<User, String> uService;
 	
 	public IBaseService<User, String> getuService() {
--- src/main/java/ilv/dao/UserDaoI.java
+++
@@ -1,9 +0,0 @@
-package ilv.dao;
-
-import ilv.model.IlvUser;
-
-import java.io.Serializable;
-
-public interface UserDaoI {
-    public Serializable save(IlvUser user);
-}
